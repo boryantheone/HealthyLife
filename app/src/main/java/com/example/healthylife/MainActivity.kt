53a7ff5c01080041ae4e1b684fa1db6bbefe6812
boryantheone <boryantheone228@yandex.ru>
2023-04-09 21:14:01 +0300

    add 7 fragments for questions, designed two fragments and main activity

--- app/src/main/java/com/example/healthylife/MainActivity.kt
+++ app/src/main/java/com/example/healthylife/MainActivity.kt
@@ -4,8 +4,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
     }
 }
